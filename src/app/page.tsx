1f1fbc2f18367bb924911ed62d8c879d392863e8
itsequal <orolon1@ucol.mx>
2024-08-15 13:28:31 -0600

    filled home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,57 @@
 // `app/page.tsx` is the UI for the `/` URL
+import Image from 'next/image'
 
 export default function Page() {
   return (
     <div>
-      <section className="text-gray-600 font-poppins">asdasd</section>
-      <section className=" text-gray-600 font-mclaren">asdasd</section>
-      <section className="text-gray-600 font-mono">asdasd</section>
+      <div className="grid grid-cols-2">
+              <div>
+                  <section className=" text-gray-600 font-mclaren h-screen flex items-center">
+                        <div className="whitespace-pre-line">
+                          <h1>Shorten those complicated URLs</h1>
+                          <p>Use our paimon URL shortener and transform those complex URLs into something  better.</p>
+                        </div>
+                  </section>
+              </div>
+              
+              <div>
+                <section className=" text-gray-600 font-mclaren h-screen flex items-center">
+                    <Image 
+                      src="/img/yoimiya_title.png"
+                      width={200}
+                      height={200}
+                      alt="Yoimiya main screen"
+                    />
+                </section>
+              </div>
+      </div>
+
+      <div className="h-screen bg-[#F8F8F8]">
+          
+          <section className=" text-gray-600 font-mclaren flex items-center justify-center">
+                <h1>
+                      Sign up and start to shorten your URLs!
+                </h1>
+          </section>
+
+          <section className="text-gray-600 font-mclaren flex items-center justify-center">
+                <div className="border-2 rounded-md border-yoimiya size-1/2">
+                    <h1>Shorten that link!</h1>
+                    <input type="text" className="border border-neutral-950 text-sm rounded-sm w-full p-2" placeholder="Example: https://www.examplelink.com"/>
+                    <button type="submit" className="text-white bg-yoimiya font-mclaren py-2 px-3 rounded-md">Shorten URL</button>
+                </div>
+          </section>
+
+
+
+
+
+      </div>
+
+
+
     </div>
+    
+
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,39 @@
 import React from "react";
-import Link from "next/link";
 import Image from "next/image";
 
 function Header() {
   return (
-    <nav className="bg-white border-gray-200 bg-yoimiya">
+    <nav className="border-gray-200 bg-yoimiya shadow-md">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
         <div className="ml-0 flex items-center space-x-3 rtl:space-x-reverse">
-             <Image src="/img/icon.png" className=""  width="72" height = "72" alt="asd" ></Image>
-             <span className="text-4xl text-white font-mclaren" >Paimon Shortener</span>
+          <Image
+            src="/img/icon.png"
+            className=""
+            width="72"
+            height="72"
+            alt="asd"
+          ></Image>
+          <span className="text-4xl text-white font-mclaren">
+            Paimon Shortener
+          </span>
         </div>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  md:bg-yoimiya">
             <li>
               <a
                 href="#"
-                className="font-mclaren block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
+                className="font-mclaren block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 aria-current="page"
               >
                 PRICING
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="font-mclaren block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                FEATURES
               </a>
             </li>
             <li>
@@ -32,12 +47,11 @@ function Header() {
             <li>
               <a
                 href="#"
-                className="font-mclaren block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                className="font-mclaren py-2 px-3 bg-white text-gray-900 hover:bg-gray-100 md:hover:bg-white rounded-md md:hover:text-blue-700 md:p-2 dark:text-yoimiya md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white"
               >
                 GET STARTED
               </a>
             </li>
-
           </ul>
         </div>
       </div>
