f8f531d6f62c1985f0b141ba30c1c26fcf50bc77
itsequal <orolon1@ucol.mx>
2024-11-27 23:37:24 -0600

    modified ignores

---
+++ back-end/src/test/java/com/paimon/shortener/ShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paimon.shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
