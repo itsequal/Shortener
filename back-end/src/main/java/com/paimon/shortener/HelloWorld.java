822cae99a3453d8925b57c932391b339c4ec3620
itsequal <orolon1@ucol.mx>
2024-11-27 23:31:08 -0600

    added backend

---
+++ back-end/src/main/java/com/paimon/shortener/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.paimon.shortener;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping("/")
+    public String greet(){
+        return "Hello, World!";
+    }
+
+
+}
